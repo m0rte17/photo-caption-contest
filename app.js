2d1bb8d1309d77a8e9b481ac9b2cb6a1051121fe
m0rte17 <alexgrudko@gmail.com>
2025-03-26 18:49:50 +0200

    Add full Swagger documentation for all API endpoints

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const swaggerUi = require('swagger-ui-express');
+const fs = require('fs');
 
 const imageRoutes = require('./routes/imageRoutes');
 const authRoutes = require('./routes/authRoutes');
+
+const swaggerDocument = JSON.parse(fs.readFileSync('./swagger.json', 'utf8'));
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/images', imageRoutes);
 app.use('/api/auth', authRoutes);
