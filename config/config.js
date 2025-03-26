9cb4cfa4a4ee5e7385466e7d79fa0ed24799a80d
m0rte17 <alexgrudko@gmail.com>
2025-03-26 23:14:21 +0200

    Add production config for Sequelize with postgres dialect

--- config/config.js
+++ config/config.js
@@ -12,6 +12,10 @@ module.exports = {
     // can be customized later
   },
   production: {
-    // customization for Render will be later
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   }
 };
